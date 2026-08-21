95700087b01844e64110830c73cf52b19e8b8a0d
Wandi Muhammad <wandimuhammad@gmail.com>
2026-08-21 16:58:46 +0700

    fix: use file session driver with ismy_session cookie

--- api/index.php
+++ api/index.php
@@ -48,6 +48,10 @@ try {
     $_ENV['APP_MAINTENANCE_DRIVER'] = 'file';
     $_SERVER['APP_MAINTENANCE_DRIVER'] = 'file';
 
+    putenv("SESSION_DRIVER=file");
+    $_ENV['SESSION_DRIVER'] = 'file';
+    $_SERVER['SESSION_DRIVER'] = 'file';
+
     if (!getenv('APP_KEY')) {
         putenv("APP_KEY=base64:DfqxP4pZlkL/m85AABaZMgC0K6AVKAMnA9TtRq4lFfc=");
         $_ENV['APP_KEY'] = 'base64:DfqxP4pZlkL/m85AABaZMgC0K6AVKAMnA9TtRq4lFfc=';
